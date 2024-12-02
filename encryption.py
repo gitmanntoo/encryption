10dba06300829f9aa209f11dbd362547f09c01c5
gitmanntoo <gitmanntoo@proton.me>
2024-12-02 10:20:43 -0500

    add noise

--- encryption.py
+++ encryption.py
@@ -1,13 +1,15 @@
 import sys
 import argparse
 import base64
+import datetime
+import getpass
+import hashlib
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-import getpass
-import hashlib
-import re
+
 
 HELP_STRING = """
 Encrypt and decrypt text using Python builtin Fernet.
@@ -94,6 +96,50 @@ def generate_key(phrases: list[str], length: int = 32) -> bytes:
     return last_key
 
 
+def do_encrypt(fernet_key: bytes, input_bytes: bytes) -> bytes:
+    """Encrypt the input text using the given key.
+    
+    Add noise with the following steps:
+    - base64 encode the input_bytes
+    - add 99 bytes from the current datetime to the beginning and end of the input_bytes
+    """
+
+    input_string = base64.urlsafe_b64encode(input_bytes).decode()
+
+    now = datetime.datetime.now()
+    before_string = hashlib.sha512(str(now).encode()).hexdigest()[:99]
+
+    now = datetime.datetime.now()
+    after_string = hashlib.sha512(str(now).encode()).hexdigest()[:99]
+
+    input_string = f"{before_string}{input_string}{after_string}"
+
+    f = Fernet(fernet_key)
+    return f.encrypt(input_string.encode())
+
+
+def do_decrypt(fernet_key: bytes, input_bytes: bytes) -> bytes:
+    """Decrypt the input text using the given key.
+    - Remove some noise from the first and last 99 characters.
+    """
+
+    f = Fernet(fernet_key)
+    
+    decrypted_string = f.decrypt(input_bytes).decode()
+    decrypted_string = decrypted_string[99:-99]
+
+    return base64.urlsafe_b64decode(decrypted_string.encode())
+
+
+def print_output(out: str):
+    """Print the output to stdout with separators before and after."""
+
+    print()
+    print("=" * 5, "BEGIN OUTPUT", "=" * 5)
+    print(out)
+    print("=" * 5, "END OUTPUT", "=" * 5)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description=HELP_STRING,
@@ -141,16 +187,15 @@ def main():
     fernet_key = generate_key(secrets)
 
     if args.gen_key:
-        print(fernet_key.decode())
+        print_output(fernet_key.decode())
         return
 
     if not input_bytes:
         input_bytes = get_multiline_input().encode()
     if not args.decrypt:
         # Encrypt data using the given key.
-        f = Fernet(fernet_key)
+        encrypted_bytes = do_encrypt(fernet_key, input_bytes)
 
-        encrypted_bytes = f.encrypt(input_bytes)
         if not args.no_base64:
             encrypted_bytes = base64.urlsafe_b64encode(encrypted_bytes)
 
@@ -158,21 +203,20 @@ def main():
             with open(args.output, "wb") as f:
                 f.write(encrypted_bytes)
         else:
-            print(encrypted_bytes.decode())
+            print_output(encrypted_bytes.decode())
 
     elif args.decrypt:
         # Decrypt data using the given key.
-        f = Fernet(fernet_key)
-
         if not args.no_base64:
             input_bytes = base64.urlsafe_b64decode(input_bytes)
-        decrypted_bytes = f.decrypt(input_bytes)
+
+        decrypted_bytes = do_decrypt(fernet_key, input_bytes)
 
         if args.output is not None:
             with open(args.output, "wb") as f:
                 f.write(decrypted_bytes)
         else:
-            print(decrypted_bytes.decode())
+            print_output(decrypted_bytes.decode(errors="ignore"))
 
 
 if __name__ == "__main__":
